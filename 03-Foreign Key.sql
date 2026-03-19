2a5164e36e6b6cb7aa7da67813172193507c596b
firoz.nur <firoznur5@gmail.com>
2026-03-19 21:51:51 +0600

    Clone : 0245-Postgres-L2_M02_m_009-D_18_05_2025

---
+++ 03-Foreign Key.sql
@@ -0,0 +1,61 @@
+CREATE Table "user" (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(25) NOT NULL
+)
+
+CREATE Table post (
+    id SERIAL PRIMARY KEY,
+    title text NOT NULL,
+    user_id INTEGER REFERENCES "user" (id) on delete set DEFAULT DEFAULT 2
+)
+
+ALTER Table post alter COLUMN user_id set NOT null;
+
+INSERT INTO
+    "user" (username)
+VALUES ('akash'),
+    ('batash'),
+    ('sagor'),
+    ('nodi');
+
+INSERT INTO
+    post (title, user_id)
+VALUES (
+        'Enjoying a sunny day with Akash! ☀️',
+        2,
+    ),
+    (
+        'Batash just shared an amazing recipe! 🍲',
+        1
+    ),
+    (
+        'Exploring adventures with Sagor.🌟',
+        4
+    ),
+    (
+        'Nodi''s wisdom always leaves me inspired. 📚',
+        4
+    );
+
+DROP Table post;
+
+DROP Table "user";
+
+SELECT * from "user";
+
+SELECT * from post;
+
+INSERT INTO post (title, user_id) VALUES ('test', NULL)
+
+-- Insertion constraint on INSERT post
+-- Attempting to insert a post with a user ID that does not exist
+-- Inserting a post with a valid user ID
+-- Attempting to insert a post without specifying a user ID
+
+DELETE FROM "user" WHERE id = 4;
+
+-- Deletion constraint on DELETE user
+-- Restrict Deletion -> ON DELETE RESTRICT / ON DELETE NO ACTION (default)
+-- Cascading Deletion -> ON DELETE CASCADE
+-- Setting NULL -> ON DELETE SET NULL
+-- Set Default value -> ON DELETE SET DEFAULT
